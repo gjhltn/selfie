de1033c68c24774eef4f99c09a80ba41460cd92e
g j h <guy@swirrl.com>
2022-10-20 19:24:33 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+import { Root } from './components/Root'
 import { Selfie } from './components/Selfie'
 
-const App = () => <Selfie />
+const router = createHashRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <div>Routing error!</div>
+	},
+	{
+		path: '/configure',
+		element: <Selfie />
+	}
+])
+
+const App = () => <RouterProvider router={router} />
 
 export default App
