31f7fb07b98e765c743576c18a461f6a066b429a
gswirrl <guy@swirrl.com>
2022-10-25 00:48:44 +0100

    use env vars

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -17,7 +17,8 @@ export const Root = () => (
 	<All colour={CONFIG.backgroundColour}>
 		<h1>Hello world</h1>
 		<p>The regular website goes here.</p>
-		<p>{  " #{REPO} " } </p>
+		<p>{  " #{REPO}# " } </p>
+		<p>{  " #{EMAIL}# " } </p>
 		{ " #{CDN}# and #{CALLBACK}# "}
 	</All>
 )
